adb7fec406453e8756a17e99d02ce0fe160e5630
Maureen Joyce <mjoyce10@kent.edu>
2020-09-21 14:43:57 -0400

    asks pet's name and species

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -7,6 +7,17 @@ namespace VirtualPet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! Welcome to Virtual Pets");
+            Pet userPet = new Pet();
+
+            Console.WriteLine("What is your pet's name?");
+            userPet.Name = Console.ReadLine();
+            Console.WriteLine($"What kind of animal is {userPet.Name}?");
+            userPet.Species = Console.ReadLine();
+
+            Console.WriteLine($"{userPet.Name} the {userPet.Species}:");
+            Console.WriteLine($"Health: {userPet.GetHealth()}");
+
+            Console.ReadLine();
         }
     }
 }
